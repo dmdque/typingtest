b5bdb20d0854944b2f4a49bf6d1b95483513828c
Daniel Que <daniel2que@hotmail.com>
2013-10-24 21:25:43 -0400

    main.cc push

---
+++ main.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+#include <termios.h>
+using namespace std;
+
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
+
+int main(){
+  string filename = "input.txt";
+  ifstream file;
+  file.open(filename.c_str()); // .c_str());
+
+  stringstream ss;
+
+  string s;
+  getline(file, s); // file >> filename; // ss;
+  cout << s.c_str() << endl; // ss.str();
+
+  char ch;
+  do{
+    ch = getch();
+    cout << ch ;//  << '\n'; // cout waits for a newline char before printing :(
+    cout.flush();
+    // cout << ss.str();
+  } while (ch != '.');
+  cout << endl;
+}
