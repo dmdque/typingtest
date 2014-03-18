beb6af4b4917878cf8eea0503adcdf2ddd24b085
dmdque <daniel2que@hotmail.com>
2014-03-18 16:36:16 -0400

    indented

--- main.cc
+++ main.cc
@@ -10,23 +10,23 @@
 using namespace std;
 
 char getch() {
-        char buf = 0;
-        struct termios old = {0};
-        if (tcgetattr(0, &old) < 0)
-                perror("tcsetattr()");
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
-        old.c_cc[VMIN] = 1;
-        old.c_cc[VTIME] = 0;
-        if (tcsetattr(0, TCSANOW, &old) < 0)
-                perror("tcsetattr ICANON");
-        if (read(0, &buf, 1) < 0)
-                perror ("read()");
-        old.c_lflag |= ICANON;
-        old.c_lflag |= ECHO;
-        if (tcsetattr(0, TCSADRAIN, &old) < 0)
-                perror ("tcsetattr ~ICANON");
-        return (buf);
+  char buf = 0;
+  struct termios old = {0};
+  if (tcgetattr(0, &old) < 0)
+    perror("tcsetattr()");
+  old.c_lflag &= ~ICANON;
+  old.c_lflag &= ~ECHO;
+  old.c_cc[VMIN] = 1;
+  old.c_cc[VTIME] = 0;
+  if (tcsetattr(0, TCSANOW, &old) < 0)
+    perror("tcsetattr ICANON");
+  if (read(0, &buf, 1) < 0)
+    perror ("read()");
+  old.c_lflag |= ICANON;
+  old.c_lflag |= ECHO;
+  if (tcsetattr(0, TCSADRAIN, &old) < 0)
+    perror ("tcsetattr ~ICANON");
+  return (buf);
 }
 
 double calc_wpm(double t, string in_s){
@@ -130,10 +130,10 @@ int main(int argc, char **argv){
         cout.flush();
         i++;
       } else if (ch == 27){ // ESC/DEL
-      // (ch == 9 || ch == 11 || ch == '\t' || ch == '\v' || ch == '\n' || ch == 92 || ch == '\\' || ch == 27)
-          // line++;
-          // goto end_loop;
-          break;
+        // (ch == 9 || ch == 11 || ch == '\t' || ch == '\v' || ch == '\n' || ch == 92 || ch == '\\' || ch == 27)
+        // line++;
+        // goto end_loop;
+        break;
       } else if (ch == 9){ // horizontal TAB
         if (' ' == sentences[line][i]){
           if (' ' == sentences[line][i+1]){ // **error check this for EOF
